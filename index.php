f9033a6ce09263f7d2be91bed6f05d92ffae2ca8
Tessakam <tessa.kam@hotmail.com>
2020-11-05 14:12:45 +0100

    permission denied solved!

--- index.php
+++ index.php
@@ -21,11 +21,17 @@ require 'buttons.html';
 require 'vendor/monolog/monolog/src/Monolog/Handler/NativeMailerHandler.php';
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
+use Monolog\Handler\BrowserConsoleHandler;
 use Monolog\Handler\NativeMailerHandler;
 
 // create a log channel
+
+$log = new Logger('Loginfo');
+$log->pushHandler(new StreamHandler('logs/info.log', Logger::INFO)); // links to Logger.php
+$log->pushHandler(new BrowserConsoleHandler(Logger::INFO));
+
 $log = new Logger('LogMessage1');
-$log->pushHandler(new StreamHandler('logs/warning.log', Logger::WARNING)); // links to Logger.php
+$log->pushHandler(new StreamHandler('logs/warning.log', Logger::WARNING));
 
 $log = new Logger('LogMessage2');
 $log->pushHandler(new StreamHandler('logs/danger.log', Logger::EMERGENCY));
@@ -37,10 +43,15 @@ $log->pushHandler(new StreamHandler('logs/emergency.log', Logger::ERROR));
 $buttonName = $_GET['type'];
 $inputMessage = $_GET['message'];
 
-// add records to the log
-$log->warning('testtest');
+// add records to the log - use Switch / case
+
+
+var_dump($log);
+$log->info($inputMessage);
+$log->warning($inputMessage);
 $log->emergency($inputMessage);
 $log->error($inputMessage);
+
 
 
 
